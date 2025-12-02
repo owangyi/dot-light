85ae335aca961ec7d6159692fb9e50efa956d11b
Yi <wangyiicloud@icloud.com>
2025-12-02 19:36:38 +0800

    patch redis ext

---
+++ applications/redis.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+done_file="$HOME/.$(basename ${BASH_SOURCE[0]}).done"
+
+if [ -f "$done_file" ]; then
+    return
+fi
+
+color_print "[ Installing Redis ]" $yellow   
+
+brew install redis
+brew services start redis
+
+touch "$done_file"
+
